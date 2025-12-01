83b84d41211c11607d5f32c5734e0dcb517835b9
SeenApologic <fin666.test@gmail.com>
2025-12-01 20:54:45 +0700

    Add files via upload

---
+++ ├── Labsheet02/Lab201.java
@@ -0,0 +1,11 @@
+public class Lab201 {
+    static void main(String[] args) {
+        int num1 = 10, num2 = 8;
+
+        System.out.println("Summation = " + (num1 + num2));
+        System.out.println("Subtraction = " + (num1 - num2));
+        System.out.println("Multiplication = " + (num1 * num2));
+        System.out.println("Division = " + (double) num1 / num2);
+        System.out.println("Modulus = " + (num1 % num2));
+    }
+}
