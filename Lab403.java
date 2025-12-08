1c4edeae560a88a26b4216242fbd54302f027b1d
SeenApologic <fin666.test@gmail.com>
2025-12-08 16:39:36 +0700

    Add files via upload

---
+++ Lab403.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Lab403 {
+    public static void main(String[] args) {
+        final String USERNAME = "admin", PASSWORD = "Admin1234";
+
+        String user = JOptionPane.showInputDialog("Enter username:");
+        String password = JOptionPane.showInputDialog("Enter password:");
+
+        boolean userPass = user.equalsIgnoreCase(USERNAME);
+        boolean passwordPass = password.equals(PASSWORD);
+
+        if ()
+    }
+}
